36d2e9557b35111dfa90534da244b1bf2f6be2c5
Selukov Andrey <selukov.andrey@gmail.com>
2019-10-19 03:21:14 +0700

    trying again

--- wpcf7-pdf-forms.php
+++ wpcf7-pdf-forms.php
@@ -1,2 +1,13 @@
 <?php
+/*
+Plugin Name: PDF Forms Filler for Contact Form 7
+Plugin URI: https://github.com/maximum-software/wpcf7-pdf-forms
+Description: Create Contact Form 7 forms from PDF forms.  Get PDF forms filled automatically and attached to email messages upon form submission on your website.  Embed images in PDF files.  Uses Pdf.Ninja API for working with PDF files.  See tutorial video for a demo.
+Version: 1.1.3
+Author: Maximum.Software
+Author URI: https://maximum.software/
+Text Domain: wpcf7-pdf-forms
+Domain Path: /languages
+License: GPLv3
+*/
 require_once ( dirname( __FILE__ ) )."pdf-forms-for-contact-form-7.php"
